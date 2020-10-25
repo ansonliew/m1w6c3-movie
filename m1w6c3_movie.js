8a291b86fa7b2a0da876d8d242f47d29a1fc36a8
ansonliew <57343638+ansonliew@users.noreply.github.com>
2020-10-25 13:47:53 +0800

    Update m1w6c3_movie.js

--- m1w6c3_movie.js
+++ m1w6c3_movie.js
@@ -1,5 +1,6 @@
 var movies = [];
-var selectedMovie
+var selectedMovie;
+var moviesDetail = [];
 
 function buttonPressed(){
 
@@ -75,18 +76,40 @@ function buttonPressed(){
 
 		newTr.movie = movies[i];
 		newTr.addEventListener('click', function(evt){
+			
+
 			selectedMovie = evt.currentTarget.movie
 			document.getElementById("movieImage").setAttribute("src", selectedMovie["Poster"])
 			document.getElementById("title").innerHTML = selectedMovie["Title"];
 			document.getElementById("year").innerHTML = selectedMovie["Year"];
 			document.getElementById("imdbID").innerHTML = selectedMovie["imdbID"];
-			document.getElementById("type").innerHTML = selectedMovie["Type"]
-			document.getElementById("poster").innerHTML = selectedMovie["Poster"]
+			document.getElementById("type").innerHTML = selectedMovie["Type"];
+			document.getElementById("poster").innerHTML = selectedMovie["Poster"];
 
-			var info = "More Movie Info"
-			var result = info.link("http://www.omdbapi.com/?i=" + selectedMovie["imdbID"] + "tt1201607&apikey=87d10179");
+
+			//This 2nd fetch API put here in order to get the selectedMovie["imdbID"] above
+			fetch("http://www.omdbapi.com/?i=" + selectedMovie["imdbID"] + "&apikey=87d10179")
+			.then(response => response.json())
+			.then(data => {
+
+				var Rated = data["Rated"]
+				var Released = data["Released"]
+				var Director = data["Director"]
+				var Awards = data["Awards"]
+
+			document.getElementById("rated").innerHTML = Rated;
+			document.getElementById("released").innerHTML = Released;
+			document.getElementById("director").innerHTML = Director;
+			document.getElementById("awards").innerHTML = Awards;
+
+			//Create the hyperlink
+			var info = "More Movie Info";
+			var result = info.link("http://www.omdbapi.com/?i=" + selectedMovie["imdbID"] + "&apikey=87d10179");
 			document.getElementById("moreLink").innerHTML = info.link("http://www.omdbapi.com/?i=" + selectedMovie["imdbID"] + "&apikey=87d10179");
-		})
+			});
+			
+
+		});
 
 		//append tr to body
 		document.getElementById("movieBody").appendChild(newTr)
